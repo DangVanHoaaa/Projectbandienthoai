32afe6c488d13351c5503f9e1434803f18867c56
Van Hoa <hoadang13737@gmail.com>
2026-07-02 14:43:22 +0700

    Initial Express MVC project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = require("./routers/client/index.route");
+const app = express();
+const port = 3000;
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+router(app);
+
+
+app.listen(port, ()=>{
+    console.log(`app listen at prot ${port}`);
+})
+
+module.exports = router;
